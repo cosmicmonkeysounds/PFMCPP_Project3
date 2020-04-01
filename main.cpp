9255dc0370ed39ba3e9927b4409babdc45418226
John Janigan-Mills <cosmicmonkeysounds@gmail.com>
2020-04-01 14:06:26 +0000

    Finished Step 2 and onward.

--- main.cpp
+++ main.cpp
@@ -105,7 +105,6 @@ struct CarWash //2)
     Car carBeingServiced;  
 };
 
-
 /*
 1)
 Mixing board
@@ -119,7 +118,28 @@ Mixing board
     1) mix channels together on master buss
     2) mix and send signals to buss'
     3) amplify/attenuate inputs
- */
+*/
+
+struct MixingBoard
+{
+    // 1. number of input channels 
+    unsigned int numberOfChannels = 16;
+    // 2. number of routable mix busses 
+    unsigned int numberOfBusses = 4;
+    // 3. if it includes internal effects or not 
+    bool doesItHaveFX = false;
+    // 4. does it include an EQ per channel?
+    bool doesItHaveEQ = true;
+    // 5. does it have compressors for each channel?
+    bool doesItHaveCompression = false;
+
+    // 1. mixes channels together on master buss
+    void processMasterBuss(void* channelData);
+    // 2. mixes and sends signals to buss
+    void sendToBuss(void* channelData, unsigned int bussSelect);
+    // 3. changes the gain of signals 
+    void changeGain(void* channelData, float gain);
+};
 
 /*
 2)
@@ -134,7 +154,29 @@ Oscilloscope
     1) count frequency of input
     2) display waveform
     3) attenuate/amplify waveform
- */
+*/
+
+struct Oscilloscope
+{
+    // 1. Number of BNC inputs
+    unsigned int numOfBNC = 2;
+    // 2. Is this an analog scope?
+    bool isThisScopeAnalog = true;
+    // 3. Maximum input voltage 
+    float maxInputVoltage = 130.5f;
+    // 4. Number of logic analyzer channels
+    unsigned int numOfLogicChannels = 0;
+    // 5. Time constant on x-axis
+    float xAxisTimeConstant = 96.5f;
+
+
+    // 1. Count frequency of inputs
+    float countFrequency(void* inputVoltage);
+    // 2. Display input waveforms 
+    void displayWaveform(void* inputVoltage);
+    // 3. amplify/attenuate input signals
+    void changeGain(void* inputVoltage, float gain);
+};
 
 /*
 3)
@@ -149,7 +191,29 @@ Television
     1) turn on and off
     2) receive video from ports 
     3) control it's pixels
- */
+*/
+
+struct Television
+{
+    // 1) size of screen
+    float screenWidth;
+    // 2) aspect ratio (integer that selects the ratio)
+    unsigned int aspectRatioSelect;
+    // 3) number of HDMI ports
+    unsigned int numOfHDMI = 2;
+    // 4) number of optical ports
+    unsigned int numOfOptical = 1;
+    // 5) is it smart?
+    bool isItSmart = false;
+
+
+    // 1) Turn on or off
+    void powerCycle();
+    // 2) receive video from ports
+    void readPorts(void* data);
+    // 3) control it's pixels
+    void controlPixel(unsigned int pixelNum, float brightness);
+};
 
 /*
 4)
@@ -164,7 +228,29 @@ Speakers
     1) output sound 
     2) filter input through crossover(s)
     3) set volume 
- */
+*/
+
+struct Speaker
+{
+    // 1) How many drivers?
+    unsigned int numOfDrivers = 2;
+    // 2) Lowest frequency reproducable at -3dB
+    float minFrequency = 45.4f;
+    // 3) Highest frequency reproducable at -3dB
+    float maxFrequency = 19500.51f;
+    // 4) is it active?
+    bool isItActive = true;
+    // 5) Power rating
+    float powerRating = 80.f;
+
+    // 1) output sound
+    void outputSound(void* signal);
+    // 2) filter input through crossover
+    void* applyCrossover(void* input); // was thinking this would return data that would be the filtered version of the input, which would leave it up to you how you'd like to store and use the filtered audio. 
+
+    // 3) set volume
+    void setVolume(float volume);
+};
 
 /*
 5)
@@ -179,7 +265,28 @@ Oscillator
     1) read and quantize CV
     2) output sound
     3) mix voices together
- */
+*/
+
+struct Oscillator
+{
+    // 1) waveform select
+    unsigned int waveform = 0;
+    // 2) frequency of oscillation
+    float frequency = 440.f;
+    // 3) amplitude of signal
+    float amplitude = 1.f;
+    // 4) FM enable
+    bool FMEnable = false;
+    // 5) number of voices
+    unsigned int numOfVoices = 2;
+
+    // 1) read and quantize CV
+    void* readCV(void* CVSignal); 
+    // 2) output sound
+    void outputSound(void* audioSignal);
+    // 3) mix voices together 
+    void* mixVoices(void* voiceSignals);
+};
 
 /*
 6)
@@ -194,7 +301,28 @@ LFO
     1) read CV
     2) output CV 
     3) retrigger LFO
- */
+*/
+
+struct LFO
+{
+    // 1) value of tempo
+    float tempo = 120.f;
+    // 2) swing amount 
+    float swingAmount = 50.f;
+    // 3) waveform select
+    unsigned int waveform = 0;
+    // 4) minimum speed
+    float minSpeed = 0.0001f;
+    // 5) maximum speed
+    float maxSpeed = 255.f;
+
+    // 1) read CV
+    void* readCV(void* CVInput);
+    // 2) outputCV
+    void outputCV(); // this takes no arguments because everything it needs to output is held in the member variables
+    // 3) retrigger LFO
+    void retrigger();
+};
 
 /*
 7)
@@ -209,7 +337,37 @@ Sequencer
     1) write/record sequence 
     2) play sequence 
     3) output CV
- */
+*/
+
+struct Sequencer
+{
+    // 1) Number of steps
+    unsigned int numOfSteps = 16;
+    // 2) restart sequence button
+    bool restartSequence = false;
+    // 3) value of tempo
+    float tempo = 89.5f;
+    // 4) swing amount
+    float swing = 45.2f;
+    // 5) velocity value
+    float velocity = 90.5f;
+
+    // Nested UDT #1
+    struct Sequence
+    {
+        void* data;
+        void* instruments;
+
+        void* quantize(void* input);
+    };
+
+    // 1) write/record sequence
+    Sequence writeRecordSequence(void* input);
+    // 2) play sequence
+    void playSequence(Sequence seq);
+    // 3) output CV
+    void outputCV(Sequence sequence);
+};
 
 /*
 8)
@@ -224,7 +382,28 @@ VCA
     1) attenuate or increase gain
     2) read CV
     3) mix inputs/outputs together
- */
+*/
+
+struct VCA
+{
+    // 1) number of inputs
+    unsigned int numOfInputs = 2;
+    // 2) number of outputs
+    unsigned int numOfOutputs = 1;
+    // 3) input signal strength
+    float inputStrength = 87.5f;
+    // 4) output signal strength
+    float outputStrength = 20.5f;
+    // 5) distortion threshold
+    float clippingThresh = 60.55f;
+
+    // 1) attenuate or increase gain
+    void* changeGain(void* input);
+    // 2) read CV
+    void* readCV(void* CVInput);
+    // 3) mix inputs/outputs together
+    void* mixSignals(void* signals);
+};
 
 /*
 9)
@@ -239,7 +418,37 @@ Filter
     1) read CV
     2) apply filter to input
     3) mix outputs together
- */
+*/
+
+struct Filter
+{
+    // 1) has low pass output
+    bool hasLowPass = true;
+    // 2) has band pass output
+    bool hasBandPass = true;
+    // 3) has high pass output
+    bool hasHighPass = true;
+    // 4) cutoff frequency value
+    float cutoffFreq = 1000.f;
+    // 5) resonance value
+    float resonance = 0.7f;
+
+    // Nested UDT #2
+    struct LowPass
+    {
+        float cutoffFreq, resonance;
+        unsigned int topology;
+
+        void* transferFunction(void* input);
+    };
+
+    // 1) read CV
+    void readCV(void* CVInput);
+    // 2) apply filter to input
+    void* applyFilter(void* input);
+    // 3) mix outputs together
+    void* mixOutputs(void* outputs);
+};
 
 /*
 10)
@@ -259,7 +468,28 @@ Synthesizer
     1) output sounds
     2) read CV 
     3) route CV in mod matrix
- */
+*/
+
+struct Synthesizer
+{
+    // 1) has a filter
+    Filter filter;
+    // 2) has a VCA
+    VCA vca;
+    // 3) has a sequencer
+    Sequencer sequencer;
+    // 4) has a LFO
+    LFO lfo;
+    // 5) has an oscillator
+    Oscillator oscillator;
+
+    // 1) outputs sounds
+    void outputSound(void* signal);
+    // 2) reads CV
+    void* readCV(void* CVInput);
+    // 3) routes CV in mod matrix
+    void routeCV(void* CVInput, unsigned int modMatrixDestination);
+};
 
 #include <iostream>
 int main()
